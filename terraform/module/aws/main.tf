d97b7b02bb4d8617a52680f2c65b470fa69963bc
Sathiyaraj Sridhar <sathiyas20@gmail.com>
2023-07-07 21:07:44 +0530

    Add Terraform resource for Amazon EKS cluster
    I have made updates to the Terraform AWS module configuration,
    enabling the creation of an EKS cluster and its associated
    dependent resources like cluster role, subnets, and security groups.

--- terraform/module/aws/main.tf
+++ terraform/module/aws/main.tf
@@ -39,6 +39,11 @@ resource "aws_iam_role" "iam_ec2_rl" {
       }]
     })
   }
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ]
   path = "/"
   tags = {
     Name = "iam-ec2-rl"
@@ -52,6 +57,28 @@ resource "aws_iam_instance_profile" "iam_ec2_rl_inst_pf" {
   path = "/"
   tags = {
     Name = "iam-ec2-rl-inst-pf"
+    Environment = var.env
+    Region = data.aws_region.current.name
+  }
+}
+resource "aws_iam_role" "iam_eks_rl" {
+  name = "iam-eks-rl"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  ]
+  path = "/"
+  tags = {
+    Name = "iam-eks-rl"
     Environment = var.env
     Region = data.aws_region.current.name
   }
@@ -226,6 +253,82 @@ resource "aws_route_table_association" "vpc_loadbalancer_sn_2_rtt_ass" {
   subnet_id = aws_subnet.vpc_loadbalancer_sn_2.id
   route_table_id = aws_route_table.vpc_pub_rtt.id
 }
+resource "aws_subnet" "vpc_eks_cp_sn_1" {
+  depends_on = [aws_vpc.vpc_net]
+  vpc_id = aws_vpc.vpc_net.id
+  cidr_block = "10.0.7.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+  tags = {
+    Name = "vpc-eks-cp-sn-1"
+    Environment = var.env
+    Region = data.aws_region.current.name
+  }
+}
+resource "aws_route_table_association" "vpc_eks_cp_sn_1_rtt_ass" {
+  depends_on = [
+    aws_subnet.vpc_eks_cp_sn_1,
+    aws_route_table.vpc_pub_rtt
+  ]
+  subnet_id = aws_subnet.vpc_eks_cp_sn_1.id
+  route_table_id = aws_route_table.vpc_pub_rtt.id
+}
+resource "aws_subnet" "vpc_eks_cp_sn_2" {
+  depends_on = [aws_vpc.vpc_net]
+  vpc_id = aws_vpc.vpc_net.id
+  cidr_block = "10.0.8.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+  tags = {
+    Name = "vpc-eks-cp-sn-2"
+    Environment = var.env
+    Region = data.aws_region.current.name
+  }
+}
+resource "aws_route_table_association" "vpc_eks_cp_sn_2_rtt_ass" {
+  depends_on = [
+    aws_subnet.vpc_eks_cp_sn_2,
+    aws_route_table.vpc_pub_rtt
+  ]
+  subnet_id = aws_subnet.vpc_eks_cp_sn_2.id
+  route_table_id = aws_route_table.vpc_pub_rtt.id
+}
+resource "aws_subnet" "vpc_eks_nd_sn_1" {
+  depends_on = [aws_vpc.vpc_net]
+  vpc_id = aws_vpc.vpc_net.id
+  cidr_block = "10.0.9.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+  tags = {
+    Name = "vpc-eks-nd-sn-1"
+    Environment = var.env
+    Region = data.aws_region.current.name
+  }
+}
+resource "aws_route_table_association" "vpc_eks_nd_sn_1_rtt_ass" {
+  depends_on = [
+    aws_subnet.vpc_eks_nd_sn_1,
+    aws_route_table.vpc_pvt_rtt
+  ]
+  subnet_id = aws_subnet.vpc_eks_nd_sn_1.id
+  route_table_id = aws_route_table.vpc_pvt_rtt.id
+}
+resource "aws_subnet" "vpc_eks_nd_sn_2" {
+  depends_on = [aws_vpc.vpc_net]
+  vpc_id = aws_vpc.vpc_net.id
+  cidr_block = "10.0.10.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+  tags = {
+    Name = "vpc-eks-nd-sn-2"
+    Environment = var.env
+    Region = data.aws_region.current.name
+  }
+}
+resource "aws_route_table_association" "vpc_eks_nd_sn_2_rtt_ass" {
+  depends_on = [
+    aws_subnet.vpc_eks_nd_sn_2,
+    aws_route_table.vpc_pvt_rtt
+  ]
+  subnet_id = aws_subnet.vpc_eks_nd_sn_2.id
+  route_table_id = aws_route_table.vpc_pvt_rtt.id
+}
 resource "aws_security_group" "vpc_kubernetes_sg" {
   depends_on = [
     aws_vpc.vpc_net,
@@ -314,6 +417,28 @@ resource "aws_security_group" "vpc_loadbalancer_sg" {
     Region = data.aws_region.current.name
   }
 }
+resource "aws_security_group" "vpc_eks_sg" {
+  depends_on = [aws_vpc.vpc_net]
+  vpc_id = aws_vpc.vpc_net.id
+  name = "vpc-eks-sg"
+  ingress {
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    self = true
+  }
+  egress {
+    protocol = "-1"
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "vpc-eks-sg"
+    Environment = var.env
+    Region = data.aws_region.current.name
+  }
+}
 resource "aws_s3_bucket" "s3_osw_chef_bkt" {
   bucket = "${var.aws_s3_bucket_prefix}-${var.env}-s3-osw-chef-bkt"
   acl = "private"
@@ -383,99 +508,123 @@ resource "aws_instance" "ec2_nat_sr_1" {
     Region = data.aws_region.current.name
   }
 }
-resource "aws_opsworks_stack" "osw_kubernetes_stk" {
+resource "aws_eks_cluster" "eks-ct" {
   depends_on = [
-    aws_iam_instance_profile.iam_ec2_rl_inst_pf,
-    aws_vpc.vpc_net,
-    aws_subnet.vpc_kubernetes_sn_2,
-    aws_s3_bucket.s3_osw_chef_bkt
+    aws_iam_role.iam_eks_rl,
+    aws_subnet.vpc_eks_cp_sn_1,
+    aws_subnet.vpc_eks_cp_sn_2,
+    aws_subnet.vpc_eks_nd_sn_1,
+    aws_subnet.vpc_eks_nd_sn_2,
+    aws_security_group.vpc_eks_sg
   ]
-  name = "osw-kubernetes-stk"
-  region = data.aws_region.current.name
-  default_availability_zone = "${data.aws_region.current.name}b"
-  color = "rgb(186, 65, 50)"
-  service_role_arn = join(":",[
-    "arn:aws:iam:",
-    data.aws_caller_identity.current.account_id,
-    "role/aws-opsworks-service-role"
-  ])
-  default_instance_profile_arn = aws_iam_instance_profile.iam_ec2_rl_inst_pf.arn
-  vpc_id = aws_vpc.vpc_net.id
-  default_subnet_id = aws_subnet.vpc_kubernetes_sn_2.id
-  use_opsworks_security_groups = false
-  default_os = "Custom"
-  default_root_device_type = "ebs"
-  hostname_theme = "Layer_Dependent"
-  default_ssh_key_name = "ec2-key-pair"
-  agent_version = "LATEST"
-  configuration_manager_name = "Chef"
-  configuration_manager_version = "12"
-  use_custom_cookbooks = true
-  custom_cookbooks_source {
-    type = "s3"
-    url = join("/",[
-      "https://s3.amazonaws.com",
-      aws_s3_bucket.s3_osw_chef_bkt.id,
-      "cookbooks.tar.gz"
-    ])
+  name = "eks-ct"
+  role_arn = aws_iam_role.iam_eks_rl.arn
+  vpc_config {
+    endpoint_private_access = true
+    endpoint_public_access = true
+    security_group_ids = [aws_security_group.vpc_eks_sg.id]
+    subnet_ids = [
+      aws_subnet.vpc_eks_cp_sn_1.id,
+      aws_subnet.vpc_eks_cp_sn_2.id,
+      aws_subnet.vpc_eks_nd_sn_1.id,
+      aws_subnet.vpc_eks_nd_sn_2.id
+    ]
   }
-  custom_json = jsonencode({
-    system = {
-      user = "ec2-user"
-      group = "ec2-user"
-    }
-    kubernetes = {
-      environment = var.env
-      component = "kubernetes"
-    }
-  })
-  manage_berkshelf = false
-  tags = {
-    Environment = var.env
-    Region = data.aws_region.current.name
-  }
+  version = 1.24
 }
-resource "aws_opsworks_custom_layer" "osw_kubernetes_1_lr" {
-  depends_on = [
-    aws_security_group.vpc_kubernetes_sg,
-    aws_opsworks_stack.osw_kubernetes_stk
-  ]
-  name = "osw-kubernetes-1-lr"
-  short_name = "osw-kubernetes-1-lr"
-  stack_id = aws_opsworks_stack.osw_kubernetes_stk.id
-  custom_security_group_ids = [aws_security_group.vpc_kubernetes_sg.id]
-  custom_json = jsonencode({
-    kubernetes = {
-      cluster = 1
-      token = null
-      token_hash = null
-      master = {
-        ip_address = null
-      }
-    }
-  })
-  custom_setup_recipes = ["system::setup","docker::setup","kubernetes::setup"]
-  custom_configure_recipes = []
-  custom_deploy_recipes = []
-  custom_undeploy_recipes = []
-  custom_shutdown_recipes = ["kubernetes::stop","docker::stop","system::stop"]
-  ebs_volume {
-    mount_point = "/mnt/kubernetes"
-    raid_level = "None"
-    number_of_disks = 1
-    size = 5
-    type = "gp2"
-    encrypted = true
-  }
-  auto_assign_elastic_ips = false
-  auto_assign_public_ips = false
-  auto_healing = false
-  install_updates_on_boot = false
-  instance_shutdown_timeout = 120
-  drain_elb_on_shutdown = false
-  use_ebs_optimized_instances = false
-  tags = {
-    Environment = var.env
-    Region = data.aws_region.current.name
-  }
-}
+# resource "aws_opsworks_stack" "osw_kubernetes_stk" {
+#   depends_on = [
+#     aws_iam_instance_profile.iam_ec2_rl_inst_pf,
+#     aws_vpc.vpc_net,
+#     aws_subnet.vpc_kubernetes_sn_2,
+#     aws_s3_bucket.s3_osw_chef_bkt
+#   ]
+#   name = "osw-kubernetes-stk"
+#   region = data.aws_region.current.name
+#   default_availability_zone = "${data.aws_region.current.name}b"
+#   color = "rgb(186, 65, 50)"
+#   service_role_arn = join(":",[
+#     "arn:aws:iam:",
+#     data.aws_caller_identity.current.account_id,
+#     "role/aws-opsworks-service-role"
+#   ])
+#   default_instance_profile_arn = aws_iam_instance_profile.iam_ec2_rl_inst_pf.arn
+#   vpc_id = aws_vpc.vpc_net.id
+#   default_subnet_id = aws_subnet.vpc_kubernetes_sn_2.id
+#   use_opsworks_security_groups = false
+#   default_os = "Custom"
+#   default_root_device_type = "ebs"
+#   hostname_theme = "Layer_Dependent"
+#   default_ssh_key_name = "ec2-key-pair"
+#   agent_version = "LATEST"
+#   configuration_manager_name = "Chef"
+#   configuration_manager_version = "12"
+#   use_custom_cookbooks = true
+#   custom_cookbooks_source {
+#     type = "s3"
+#     url = join("/",[
+#       "https://s3.amazonaws.com",
+#       aws_s3_bucket.s3_osw_chef_bkt.id,
+#       "cookbooks.tar.gz"
+#     ])
+#   }
+#   custom_json = jsonencode({
+#     system = {
+#       user = "ec2-user"
+#       group = "ec2-user"
+#     }
+#     kubernetes = {
+#       environment = var.env
+#       component = "kubernetes"
+#     }
+#   })
+#   manage_berkshelf = false
+#   tags = {
+#     Environment = var.env
+#     Region = data.aws_region.current.name
+#   }
+# }
+# resource "aws_opsworks_custom_layer" "osw_kubernetes_1_lr" {
+#   depends_on = [
+#     aws_security_group.vpc_kubernetes_sg,
+#     aws_opsworks_stack.osw_kubernetes_stk
+#   ]
+#   name = "osw-kubernetes-1-lr"
+#   short_name = "osw-kubernetes-1-lr"
+#   stack_id = aws_opsworks_stack.osw_kubernetes_stk.id
+#   custom_security_group_ids = [aws_security_group.vpc_kubernetes_sg.id]
+#   custom_json = jsonencode({
+#     kubernetes = {
+#       cluster = 1
+#       token = null
+#       token_hash = null
+#       master = {
+#         ip_address = null
+#       }
+#     }
+#   })
+#   custom_setup_recipes = ["system::setup","docker::setup","kubernetes::setup"]
+#   custom_configure_recipes = []
+#   custom_deploy_recipes = []
+#   custom_undeploy_recipes = []
+#   custom_shutdown_recipes = ["kubernetes::stop","docker::stop","system::stop"]
+#   ebs_volume {
+#     mount_point = "/mnt/kubernetes"
+#     raid_level = "None"
+#     number_of_disks = 1
+#     size = 5
+#     type = "gp2"
+#     encrypted = true
+#   }
+#   auto_assign_elastic_ips = false
+#   auto_assign_public_ips = false
+#   auto_healing = false
+#   install_updates_on_boot = false
+#   instance_shutdown_timeout = 120
+#   drain_elb_on_shutdown = false
+#   use_ebs_optimized_instances = false
+#   tags = {
+#     Environment = var.env
+#     Region = data.aws_region.current.name
+#   }
+# }
--- terraform/module/aws/outputs.tf
+++ terraform/module/aws/outputs.tf
@@ -6,3 +6,7 @@ output "ec2_nat_sr_1_pub_ip" {
   depends_on = [aws_instance.ec2_nat_sr_1]
   value = aws_instance.ec2_nat_sr_1.public_ip
 }
+output "eks_ct_endpoint" {
+  depends_on = [aws_eks_cluster.eks-ct]
+  value = aws_eks_cluster.eks-ct.endpoint
+}
