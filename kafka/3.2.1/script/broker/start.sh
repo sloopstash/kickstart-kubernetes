a7314b94de4f44d7c3e27cce816293a0e93be684
Sujan Kumar <sujankumar4593@gmail.com>
2024-03-21 15:31:06 +0530

    Add Kafka and Hadoop workloads
    Added Kafka and Hadoop cofig files.
    Added required templates for persistent-volume, service, stateful-set for Kafka and Hadoop.
    Added Supervisor configs for kafka and Hadoop.

---
+++ kafka/3.2.1/script/broker/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NODE_ID=$(( ${HOSTNAME:18} + 1000 ))
+SERVER_REFERENCE_CONF=/opt/kafka/conf/server-reference.conf
+SERVER_CONF=/opt/kafka/conf/server.conf
+export KAFKA_HEAP_OPTS="-Xmx256M -Xms256M"
+sed "s/node.id=.*/node.id=$NODE_ID/" $SERVER_REFERENCE_CONF > $SERVER_CONF
+/usr/local/lib/kafka/bin/kafka-storage.sh format -t $CLUSTER_ID -c $SERVER_CONF
+/usr/local/lib/kafka/bin/kafka-server-start.sh $SERVER_CONF
