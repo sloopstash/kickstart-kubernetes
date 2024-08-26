c5bdcf551810cdbe99d2ecaca5f129db9c288f02
Divya Priya Muthuvel <143541386+DivyaPriya-Muthuvel@users.noreply.github.com>
2024-08-26 13:31:31 +0530

    Project Data Lake: Setup of Apache Kafka and Apache Hadoop cluster using Kubernetes (#13)
    Enhanced the Kubernetes components to work in Mutli-Environment setup suitable for On-premise.
    Improvised the Persistent volume templates to map with the corresponding Persistent Volume Claim seamlessly.
    
    Co-authored-by: Divya Priya Muthuvel <dpriyamuthuvel4@gmailcom>

--- workload/kafka/3.2.1/broker/script/start.sh
+++ workload/kafka/3.2.1/broker/script/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+NODE_ID=$(( ${HOSTNAME:13} + 1000 ))
 SERVER_REFERENCE_CONF=/opt/kafka/conf/server-reference.conf
 SERVER_CONF=/opt/kafka/conf/server.conf
 CLUSTER_ID="OKbXWnLxQI-iFjS3AFuIzw"
 export KAFKA_HEAP_OPTS="-Xmx256M -Xms256M"
-sed "s/node.id=.*/node.id=$NODE_ID/" $SERVER_REFERENCE_CONF > $SERVER_CONF
+sed -e "s/node.id=.*/node.id=$NODE_ID/" \
+    -e "s/ENVIRONMENT/$ENVIRONMENT/g" \
+    $SERVER_REFERENCE_CONF > $SERVER_CONF
 /usr/local/lib/kafka/bin/kafka-storage.sh format -t $CLUSTER_ID -c $SERVER_CONF
 /usr/local/lib/kafka/bin/kafka-server-start.sh $SERVER_CONF
